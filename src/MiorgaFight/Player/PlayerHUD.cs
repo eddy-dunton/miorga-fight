895654d9aee2291758f8e032960356319858f057
edward-dunton <edward_dunton@btconnect.com>
2020-08-20 17:27:28 +0100

    Added sprites to player health bars

--- src/MiorgaFight/Player/PlayerHUD.cs
+++ src/MiorgaFight/Player/PlayerHUD.cs
@@ -5,12 +5,12 @@ namespace MiorgaFight {
 
 public class PlayerHUD : Control
 {
-    public ProgressBar nodeHP;
+    public TextureProgress nodeHP;
     public Label nodeScore;
 
     public override void _Ready() {
         //Get nodes
-        this.nodeHP = GetNode<ProgressBar>("pb_health");
+        this.nodeHP = GetNode<TextureProgress>("pb_health");
         this.nodeScore = GetNode<Label>("la_score");
         //Be invisible on creation
         this.Visible = false;
