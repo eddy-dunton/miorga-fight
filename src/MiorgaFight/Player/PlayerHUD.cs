0178ad5a067ad18d56fc29bfb216758556184d00
edward-dunton <edward_dunton@btconnect.com>
2020-08-20 18:14:09 +0100

    Swapped numbers for lives sprites
    And introduced #24, to be fixed before v4.0

--- src/MiorgaFight/Player/PlayerHUD.cs
+++ src/MiorgaFight/Player/PlayerHUD.cs
@@ -6,13 +6,32 @@ namespace MiorgaFight {
 public class PlayerHUD : Control
 {
     public TextureProgress nodeHP;
-    public Label nodeScore;
+    public Sprite[] lives; 
 
     public override void _Ready() {
         //Get nodes
-        this.nodeHP = GetNode<TextureProgress>("pb_health");
-        this.nodeScore = GetNode<Label>("la_score");
+        this.nodeHP = this.GetNode<TextureProgress>("pb_health");
+        
+        //Populate lives
+        this.lives = new Sprite[5];
+        for (int i = 1; i <= 5; i ++) {
+            this.lives[i - 1] = this.GetNode<Sprite>("sp_lives_" + i.ToString());
+        }
+
         //Be invisible on creation
         this.Visible = false;
+    }
+
+    public void SetLives(int lives) {
+        //Map lives to array index of this.lives (1-5 > 0-4)
+        lives --;
+        //Set each sprite number below the lives to be visible
+        for (int i = 0; i < lives; i ++) {
+            this.lives[i].Visible = true;
+        }
+        //Set all the sprites above to be invisible
+        for (int i = 4; i > lives; i --) {
+            this.lives[i].Visible = false;    
+        }
     }
 }}
