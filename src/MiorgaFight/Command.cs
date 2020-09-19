177a15c92d1ed50e2db6116aaa5504e27af2c869
Eddy Dunton <42977035+edward-dunton@users.noreply.github.com>
2020-09-19 17:59:58 +0100

    Actual fix for random number generator

--- src/MiorgaFight/Command.cs
+++ src/MiorgaFight/Command.cs
@@ -19,7 +19,7 @@ public class Command : Node {
     private static Random random = new Random();
 
     //Returns a pseudo random double between min and max (inclusive)
-    public static double Random(double min, double max) => min + (random.NextDouble() / (max - min));
+    public static double Random(double min, double max) => Command.Map(0.0, 1.0, min, max, random.NextDouble());
 
     //Returns a pseudo int double between min and max (inclusive)
     public static int Random(int min, int max) => min + (random.Next() % (max - min + 1));    
